a1012ef7bbbda7695733a191e4306218dd8ad77c
Marc Preston <phyrenight@gmail.com>
2017-11-15 19:06:01 -0500

    lint code

--- database_setup.py
+++ database_setup.py
@@ -5,6 +5,7 @@ from sqlalchemy import create_engine, ForeignKeyConstraint
 
 Base = declarative_base()
 
+
 class User(Base):
     """Contains User information.
 
@@ -13,13 +14,13 @@ class User(Base):
        :param str lastname: user's last name
        :param str password: user's password
        :param str email: user's valid email
-    
+
     """
     __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
     firstname = Column(String(250), nullable=False)
     lastname = Column(String(250), nullable=False)
-    password = Column(String(20),nullable=False)
+    password = Column(String(20), nullable=False)
     email = Column(String(250), nullable=False)  # should be unique
 
     def __init__(self, first_name, last_name, password, email):
@@ -31,32 +32,33 @@ class User(Base):
 
 
 class Contact(Base):
-    """Contains info on each contact.
+    """  Contains info on each contact.
 
        :param int contactId: unique id for the contact
        :param int userId: unique key that is taken from the User table
        :param str name: name of the contact
        :param str phoneNumber: contact's phone number
        :param str address: contact's physical address
-       :param str email: contact's valid email address 
-    
+       :param str email: contact's valid email address
+
     """
     __tablename__ = 'Contact'
     contactId = Column(Integer, primary_key=True)
-    UserId = Column(Integer, ForeignKey('user.id')) #
+    UserId = Column(Integer, ForeignKey('user.id'))
     name = Column(String)
     phoneNumber = Column(String(20))
     address = Column(String(250))
     email = Column(String(250))
-    __table_args__ = (ForeignKeyConstraint([UserId],['user.id']), {})
+    __table_args__ = (ForeignKeyConstraint([UserId], ['user.id']), {})
+
     @property
     def serialize(self):
         return {
-            'id' : self.contactId,
-            'name' : self.name,
-            'phoneNumber' : self.phoneNumber,
-            'email' : self.email,
-            'address' : self.address,
+            'id': self.contactId,
+            'name': self.name,
+            'phoneNumber': self.phoneNumber,
+            'email': self.email,
+            'address': self.address,
         }
 
 
--- forms.py
+++ forms.py
@@ -2,58 +2,74 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email, Length
 
+
 class SignUpForm(FlaskForm):
-    first_name = StringField('First name',
-                             validators=[DataRequired
-                             ('Please enter your first name')])
-    last_name = StringField('Last name', 
-                            validators=
-                                       [DataRequired('Please enter your last name')])
-    email = StringField('Email',
-                         validators=
-                                     [DataRequired('Please enter your email address'), Email('Please enter a valid email address')])
-    password = PasswordField('Password',
-                              validators=[DataRequired
-                              ('Please enter a password'),
-                               Length(min=6, 
-                               message='Password must be at least 6 characters long')])
+    first_name = StringField(
+        'First name',
+        validators=[DataRequired('Please enter your first name')])
+    last_name = StringField(
+        'Last name',
+        validators=[DataRequired('Please enter your last name')])
+    email = StringField(
+        'Email',
+        validators=[DataRequired(
+            'Please enter your email address'),
+             Email('Please enter a valid email address')])
+    password = PasswordField(
+        'Password',
+        validators=[
+            DataRequired('Please enter a password'),
+            Length(
+                min=6,
+                message='Password must be at least 6 characters long')])
     submit = SubmitField('Sign up')
 
+
 class LoginForm(FlaskForm):
-    email = StringField('Email',
-                        validators=[DataRequired
-                        ('Please enter your email address'),
-                         Email('Please enter a valid email')])
-    password = PasswordField('Password',
-                             validators=[DataRequired
-                             ('Please enter your password')])
+    email = StringField(
+        'Email',
+        validators=[
+            DataRequired('Please enter your email address'),
+            Email('Please enter a valid email')])
+    password = PasswordField(
+        'Password',
+        validators=[DataRequired('Please enter your password')])
     submit = SubmitField('Sign in')
 
+
 class ContactForm(FlaskForm):
-   first_name = StringField('First name',
-                             validators=[DataRequired
-                             ('Please enter a first name for this contact')])
-   last_name = StringField('Last name', validators=[DataRequired('Please enter a last name for this contact')])
-   phone_number = StringField('Phone number',
-                               validators=[DataRequired
-                               ('Please enter a phone number'),
-                                Length(min=6,
-                                 message='Please enter a valid phone number')])
-   email = StringField('Email')
-   address = StringField('Address')
-   submit = SubmitField('Submit contact')
+    first_name = StringField(
+        'First name',
+        validators=[
+            DataRequired('Please enter a first name for this contact')])
+    last_name = StringField(
+        'Last name',
+        validators=[
+            DataRequired('Please enter a last name for this contact')])
+    phone_number = StringField(
+        'Phone number',
+        validators=[
+            DataRequired('Please enter a phone number'),
+            Length(min=6, message='Please enter a valid phone number')])
+    email = StringField('Email')
+    address = StringField('Address')
+    submit = SubmitField('Submit contact')
+
 
 class RequestPasswordReset(FlaskForm):
-    email = StringField('Enter your email address',
-                         validators=[DataRequired
-                         ('Please enter your email address.')], render_kw={'placeholder': 'email@email.com'})
+    email = StringField(
+        'Enter your email address',
+        validators=[DataRequired('Please enter your email address.')],
+        render_kw={'placeholder': 'email@email.com'})
     submit = SubmitField('Submit request')
 
+
 class ChangePassword(FlaskForm):
-    email = StringField('Email',
-                      validators=[DataRequired
-                      ('Please enter your email.')], render_kw={"placeholder": "email@email.com"})
-    password = StringField('Password',
-                          validators=[DataRequired
-                          ("Please enter a new password")])
+    email = StringField(
+        'Email',
+        validators=[DataRequired('Please enter your email.')],
+        render_kw={"placeholder": "email@email.com"})
+    password = StringField(
+        'Password',
+        validators=[DataRequired("Please enter a new password")])
     submit = SubmitField('Submit')
